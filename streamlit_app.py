fbfc2b4865838102dcc1c3612f53670b6dc72195
krrohit049 <140319278+krrohit049@users.noreply.github.com>
2025-08-14 23:46:08 +0530

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
